6809a21e205eede3c11a0f08f12f1432ff0edcbd
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2024-02-26 11:32:54 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <p className="read-the-docs">Welcome</p>
+    </>
+  );
+}
+
+export default App;
